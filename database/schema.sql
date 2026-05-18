fb3862f8029b5201b6c01dd6fb2768035f3cd838
Fazira Naysa <faziranaysa1766@gmail.com>
2026-05-18 21:03:04 +0700

    fix: update schema database

--- database/schema.sql
+++ database/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     nama_lengkap VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    role ENUM('pengguna', 'penanggung_jawab', 'admin') DEFAULT 'pengguna',
+    role ENUM('pengguna', 'penanggung_jawab') DEFAULT 'pengguna',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
